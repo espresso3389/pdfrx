cfc9524c7826e6f26aa9d0cd65b3de6694d9c76a
Takashi Kawasaki <espresso3389@gmail.com>
2025-10-05 03:03:47 +0900

    WIP

--- packages/pdfrx_engine/lib/src/mock/string_buffer_wrapper.dart
+++ packages/pdfrx_engine/lib/src/mock/string_buffer_wrapper.dart
@@ -16,4 +16,7 @@ class StringBufferWrapper {
 ///
 /// - for native code, use [StringBuffer] directly
 /// - for Flutter Web, use this [StringBufferWrapper] that internally uses [String] instead.
-StringBufferWrapper createStringBufferForWorkaroundSafariWasm() => StringBufferWrapper();
+StringBufferWrapper createStringBufferForWorkaroundSafariWasm() {
+  // FIXME: we need some kind of kIsWeb && kIsSafari check here but it's not easy to do that correctly on pdfrx_engine.
+  return StringBufferWrapper();
+}
