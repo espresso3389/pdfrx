c3646a5de975c79fe91518889817a89b71bdb065
Takashi Kawasaki <espresso3389@gmail.com>
2025-10-18 19:51:48 +0900

    WIP

---
+++ packages/pdfrx/example/rendering_engine_diff/README.md
@@ -0,0 +1,4 @@
+# rendering_engine_diff
+
+This app is to check differences between PDFium and CoreGraphics based renderer.
+
---
+++ packages/pdfrx/example/rendering_engine_diff/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_foundation
+import pdfrx
+import pdfrx_coregraphics
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PdfrxPlugin.register(with: registry.registrar(forPlugin: "PdfrxPlugin"))
+  PdfrxCoregraphicsPlugin.register(with: registry.registrar(forPlugin: "PdfrxCoregraphicsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
