cfd828c902bc951c06557c6456a8ab408273e707
Takashi Kawasaki <espresso3389@gmail.com>
2025-11-04 09:11:41 +0900

    pdf_combine show generated PDF on another tab

--- packages/pdfrx/example/pdf_combine/lib/helper_web.dart
+++ packages/pdfrx/example/pdf_combine/lib/helper_web.dart
@@ -3,18 +3,29 @@ import 'dart:typed_data';
 
 import 'package:web/web.dart' as web;
 
-Future<void> savePdf(Uint8List bytes, {String? suggestedName}) async {
+Future<void> savePdf(Uint8List bytes, {String? suggestedName, bool openInNewTab = true}) async {
   final blob = web.Blob([bytes].jsify() as JSArray<web.BlobPart>, web.BlobPropertyBag(type: 'application/pdf'));
 
   final url = web.URL.createObjectURL(blob);
-  final anchor = web.HTMLAnchorElement();
-  anchor.href = url;
-  anchor.download = suggestedName ?? 'document.pdf';
-  web.document.body?.append(anchor);
-  anchor.click();
 
-  web.URL.revokeObjectURL(url);
-  anchor.remove();
+  if (openInNewTab) {
+    // Open in a new tab
+    web.window.open(url, '_blank');
+
+    Future.delayed(const Duration(seconds: 1), () {
+      web.URL.revokeObjectURL(url);
+    });
+  } else {
+    // Download the file
+    final anchor = web.HTMLAnchorElement();
+    anchor.href = url;
+    anchor.download = suggestedName ?? 'document.pdf';
+    web.document.body?.append(anchor);
+    anchor.click();
+
+    web.URL.revokeObjectURL(url);
+    anchor.remove();
+  }
 }
 
 const bool isWindowsDesktop = false;
