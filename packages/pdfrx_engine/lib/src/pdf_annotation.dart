6e2442d00495eb37e23b9a17e445afff707a7c7a
Babek Teymurov <bybabek@gmail.com>
2025-11-19 14:49:19 +0400

    fix: Remove Flutter dependency from PdfAnnotation class

--- packages/pdfrx_engine/lib/src/pdf_annotation.dart
+++ packages/pdfrx_engine/lib/src/pdf_annotation.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 /// PDF annotation information extracted from PDF links.
 ///
 /// Contains metadata about PDF annotations such as author, content, and dates.
-@immutable
 class PdfAnnotation {
   const PdfAnnotation({this.author, this.content, this.subject, this.modificationDate, this.creationDate});
 
@@ -31,18 +28,8 @@ class PdfAnnotation {
 
   @override
   String toString() {
-    final buffer = StringBuffer('PdfAnnotation{');
-    final fields = <String>[];
-
-    if (author != null) fields.add('author: $author');
-    if (content != null) fields.add('content: $content');
-    if (subject != null) fields.add('subject: $subject');
-    if (modificationDate != null) fields.add('modDate: $modificationDate');
-    if (creationDate != null) fields.add('createDate: $creationDate');
-
-    buffer.write(fields.join(', '));
-    buffer.write('}');
-    return buffer.toString();
+    return 'PdfAnnotation{author: $author, content: $content, subject: $subject, '
+        'modificationDate: $modificationDate, creationDate: $creationDate}';
   }
 
   @override
@@ -58,45 +45,7 @@ class PdfAnnotation {
   }
 
   @override
-  int get hashCode =>
-      author.hashCode ^ content.hashCode ^ subject.hashCode ^ modificationDate.hashCode ^ creationDate.hashCode;
-
-  /// Creates a copy with the given fields replaced with new values.
-  PdfAnnotation copyWith({
-    String? author,
-    String? content,
-    String? subject,
-    String? modificationDate,
-    String? creationDate,
-  }) {
-    return PdfAnnotation(
-      author: author ?? this.author,
-      content: content ?? this.content,
-      subject: subject ?? this.subject,
-      modificationDate: modificationDate ?? this.modificationDate,
-      creationDate: creationDate ?? this.creationDate,
-    );
-  }
-
-  /// Converts to a map for serialization.
-  Map<String, dynamic> toJson() {
-    return {
-      if (author != null) 'author': author,
-      if (content != null) 'content': content,
-      if (subject != null) 'subject': subject,
-      if (modificationDate != null) 'modificationDate': modificationDate,
-      if (creationDate != null) 'creationDate': creationDate,
-    };
-  }
-
-  /// Creates from a map for deserialization.
-  factory PdfAnnotation.fromJson(Map<String, dynamic> json) {
-    return PdfAnnotation(
-      author: json['author'] as String?,
-      content: json['content'] as String?,
-      subject: json['subject'] as String?,
-      modificationDate: json['modificationDate'] as String?,
-      creationDate: json['creationDate'] as String?,
-    );
+  int get hashCode {
+    return author.hashCode ^ content.hashCode ^ subject.hashCode ^ modificationDate.hashCode ^ creationDate.hashCode;
   }
 }
