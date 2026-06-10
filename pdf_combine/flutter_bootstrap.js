ace1005b1d0b7b41c4d10c0f78d742e2b0a5e616
GitHub Action <action@github.com>
2026-06-10 15:51:04 +0000

    94af8db1515c42c6b4c65f025d9ea4da73347c83

--- pdf_combine/flutter_bootstrap.js
+++ pdf_combine/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1005635504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2480524544" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- viewer/flutter_bootstrap.js
+++ viewer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3509843014" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "172284904" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
