e6eef15d047053f959a98025b6c84072bd3fd1b4
GitHub Action <action@github.com>
2026-06-22 19:43:48 +0000

    e457ba6e9a366c25eb8239a6fc4205905b481a87

--- viewer/flutter_bootstrap.js
+++ viewer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1526752525" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "855409708" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
