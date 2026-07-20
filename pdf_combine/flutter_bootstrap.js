72ce2733170ab76fbdd7b120da68f81ef915ad5c
GitHub Action <action@github.com>
2026-07-20 15:13:18 +0000

    9a2d64ecf6be721ab21f93f20131956227b684b8

--- pdf_combine/flutter_bootstrap.js
+++ pdf_combine/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3616445529" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "760841125" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
